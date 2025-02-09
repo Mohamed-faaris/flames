3393ff637b2a851052ab6546b2e2b1b169837595
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2025-02-10 02:01:17 +0530

    PWA-test-2

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/assets/css/style.css',
+    '/assets/icons/apple-touch-icon.png',
+    '/assets/icons/favicon-32x32.png',
+    '/assets/icons/favicon-16x16.png',
+    '/assets/icons/android-chrome-192x192.png',
+    '/assets/icons/android-chrome-512x512.png',
+    '/js/script.js',
+    '/site.webmanifest'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
